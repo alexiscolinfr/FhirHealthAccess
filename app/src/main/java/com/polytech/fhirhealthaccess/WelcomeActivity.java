805413b3734b31a91850033891586b874ee0174f
omranhaidari <omran.haidari@etu.univ-lyon1.fr>
2021-02-10 16:25:31 +0100

    Ajout de l'écran d'accueil

---
+++ app/src/main/java/com/polytech/fhirhealthaccess/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.polytech.fhirhealthaccess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    public static int TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
